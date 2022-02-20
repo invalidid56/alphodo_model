9172604f80db964cff566f052c161a0311fbc522
invalidid56 <karl2003@naver.com>
2022-02-20 23:47:24 +0900

    Data Preprocessing

--- datagen.py
+++ datagen.py
@@ -6,18 +6,16 @@ import time
 import re
 import config
 
-import PIL
-import PIL.Image
 import tensorflow as tf
+import shutil
+import pickle
 
-
-def main(data_home, initialize=False):
+def main(data_home, ex_home, initialize=False):
     # Download Data
     # 계속 보내면 423으로 막아버림, 중간중간 끊어서 다운로드 필요
-
     if initialize:
         if os.path.isdir(data_home):
-            os.rmdir(data_home)
+            shutil.rmtree(data_home)
         os.mkdir(data_home)
 
         with open('img_urls.txt') as f:
@@ -25,7 +23,7 @@ def main(data_home, initialize=False):
                 print(i)
                 try:
                     wget.download(url, data_home)
-                    time.sleep(0.8)
+                    time.sleep(1.5)
                 except urllib.error.HTTPError:
                     #time.sleep(600) # 미봉책
                     #wget.download(url, data_home)
@@ -38,27 +36,33 @@ def main(data_home, initialize=False):
     # Labeling Images
 
     reg = {
-        'ulcer': [re.compile('2007_')],
-        'ciner': [re.compile('2005_')],
-        'powder': [re.compile('2006_')],
-        'mosaic': [re.compile('IMG18'), re.compile('DSC18')],
+        'ulcer': [re.compile('IMG2007_')],
+        'ciner': [re.compile('IMG2005_')],
+        'powder': [re.compile('IMG2006_')],
+        'mosaic': [re.compile('IMG18'), re.compile('DSC18_07[69]')],
         'rancid': [re.compile('IMG19')],
-        'mildew': [re.compile('DSC18_07'), re.compile('DSC18_08'), re.compile('HLDSC')],
+        'mildew': [re.compile('DSC18_07[78]'), re.compile('DSC18_08'), re.compile('HLDSC')],
         'fungi': [re.compile('IMG_')]
     }
     diseases = reg.keys()
 
     file_list = [file for file in os.listdir(data_home) if file.endswith('.jpg')]
+    if os.path.isdir(ex_home):
+        shutil.rmtree(ex_home)
+    os.mkdir(ex_home)
+
     for disease in diseases:
-        os.mkdir(os.path.join(data_home, disease))
+        os.mkdir(os.path.join(ex_home, disease))
 
     for img in file_list:
+
         for d in diseases:
             for r in reg[d]:
                 if r.match(img):
-                    new_path = os.path.join(data_home, reg[d])
-                    os.renames(
-                         os.path.join(data_home, img), os.path.join(new_path, img)
+                    old_path = os.path.join(data_home, img)
+                    new_path = os.path.join(ex_home, d)
+                    shutil.copy(
+                            old_path, os.path.join(new_path, img)
                     )
                     break
 
@@ -66,19 +70,33 @@ def main(data_home, initialize=False):
 
     img_height = 248
     img_width = 248
+    batch_size = 16
     train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        data_home,
-        validation_split = 0.2,
-        subset = 'training',
-        seed = 111,
-        image_size = (img_height, img_width)
+        ex_home,
+        validation_split=0.2,
+        subset='training',
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size
     )
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        ex_home,
+        validation_split=0.2,
+        subset='validation',
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size
+    )
+    norm_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
 
-    # Convering Img+Lbl 2 nparray
+    train_ds = train_ds.map(lambda x, y: (norm_layer(x), y))
+    val_ds = val_ds.map(lambda x, y: (norm_layer(x), y))
+
+    return train_ds, val_ds
 
 
 if __name__ == '__main__':
-    main(config.parse()['DATA_HOME'], initialize=True)
+    main(config.parse()['DATA_HOME'], config.parse()['EX_HOME'], initialize=False)
 
 # 논문 페이지에서 이미지 다운로드 받고(DATA_HOME), nparray로 저장
 
